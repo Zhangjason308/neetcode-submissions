02b69135e8deb11088ace40fc94a9d8146fe2e5b
Zhangjason308 <96853752+Zhangjason308@users.noreply.github.com>
2026-05-27 20:46:47 -0400

    Update: is-anagram - submission-0

--- Data Structures & Algorithms/is-anagram/submission-0.py
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -1,19 +1,22 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s_arr = sorted(list(s))
-        t_arr = sorted(list(t))
         s_hash = {}
         t_hash = {}
-        for i in s_arr:
-            if i not in s_hash.keys():
-                s_hash[i] = 1
-            else: 
-                s_hash[i] = s_hash[i] + 1
-        for i in t_arr:
-            if i not in t_hash.keys():
-                t_hash[i] = 1
-            else: 
-                t_hash[i] = t_hash[i] + 1
-        if t_hash != s_hash:
-            return False
-        return True
+        for char in s:
+            if char not in s_hash:
+                s_hash[char] = 1
+            else:
+                s_hash[char] = s_hash[char] + 1
+        
+        for char in t:
+            if char not in t_hash:
+                t_hash[char] = 1
+            else:
+                t_hash[char] = t_hash[char] + 1
+        
+        if s_hash == t_hash:
+            return True
+        return False
+
+
+        
